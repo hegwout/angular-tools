1c15e9e0ad0787ae6ab734a43b91953b353e274c
hegwout <hegw@outlook.com>
2023-05-14 10:47:35 +0800

    Add cart

--- Angular.md
+++ Angular.md
@@ -1,2 +1,57 @@
-#
+# Angular commands
 ng generate component product-details
+ng generate service cart
+npm install
+npm serve
+
+ng build
+ng build --prod
+
+#
+```mermaid
+graph LR
+App --> Module --> Component
+
+Module --> Router
+Module --> FormsModule
+Module --> ReactiveFormsModule
+Module --> CommonModule
+Module --> BrowserModule
+Module --> BrowserAnimationsModule
+Module --> AppRoutingModule
+Module --> AppComponent
+Module --> HttpClientModule
+
+AppComponent --> Services
+Services --> HttpClient 
+
+Component --> selector
+Component --> templateUrl
+Component --> template
+Component --> styleUrls
+Component --> styles
+Component --> providers
+Component --> Input
+Component --> Output
+
+template --> pipe
+template --> Variable("{{}}")
+template --> onClick --> click("(click)")
+template --> onSubmit --> click("(submit)")
+template --> routerLink
+template --> ngModel
+template --> ngClass
+template --> ngStyle
+
+template --> *ngIf
+template --> *ngFor 
+template --> *ngSwitch
+
+pipe --> date
+pipe --> uppercase
+pipe --> lowercase
+pipe --> currency
+pipe --> json
+pipe --> percent
+
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
@@ -7,20 +8,29 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailsComponent,
     ProductAlertsComponent,
-    ProductListComponent
+    ProductListComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'list', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
+
     ])
   ],
   providers: [],
